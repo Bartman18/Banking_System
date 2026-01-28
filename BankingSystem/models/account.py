6aa1fdbe70e3b437fd16b46796232320056bf017
Bartosz Iwanicki <bartosz.iwanickii@gmail.com>
2026-01-28 22:51:58 +0100

    feat: fix protected balance

--- BankingSystem/models/account.py
+++ BankingSystem/models/account.py
@@ -7,11 +7,21 @@ class Account(Base):
 
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('users.id'))
-    balance = Column(Float, default=0)
+    _balance = Column(Float, default=0)
 
     def __repr__(self):
         return f"Account(id={self.id}, user_id={self.user_id}, balance={self.balance})"
 
     def __str__(self):
         return f"Account nr {self.id} (Saldo: {self.balance:.2f} PLN)"
+
+    @property
+    def balance(self):
+        return self._balance
+
+    @balance.setter
+    def balance(self, value):
+        if value < 0:
+            raise ValueError("Balance can't be negative")
+        self._balance = value
 
