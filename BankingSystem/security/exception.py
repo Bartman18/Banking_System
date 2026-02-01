5f62386f6bc26f18632cc977a94ef979c21fc15a
Bartosz Iwanicki <bartosz.iwanickii@gmail.com>
2026-02-01 22:55:25 +0100

    feat: add exception

---
+++ BankingSystem/security/exception.py
@@ -0,0 +1,18 @@
+class AccountNotFound(Exception):
+    pass
+
+
+class InvalidAmount(Exception):
+    pass
+
+
+class InsufficientFunds(Exception):
+    pass
+
+
+class UserAlreadyExists(Exception):
+    pass
+
+
+class UserNotFound(Exception):
+    pass
