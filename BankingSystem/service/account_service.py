c215d7ea5cc4e9f60e72420b9673c8793aceacb2
Bartosz Iwanicki <bartosz.iwanickii@gmail.com>
2026-02-01 22:57:26 +0100

    fix: account_service change exception and fixed

--- BankingSystem/service/account_service.py
+++ BankingSystem/service/account_service.py
@@ -1,9 +1,10 @@
-from sqlalchemy.orm import Session
-from fastapi import HTTPException
+from sqlalchemy.orm import Session, joinedload
 from datetime import datetime
 from models.account import Account
 from models.transaction import Transaction
 from models.enums import TransactionType
+from security.exception import AccountNotFound, InsufficientFunds, InvalidAmount
+from service.transaction_service import TransactionService
 
 
 class AccountService:
@@ -13,30 +14,25 @@ class AccountService:
     def get_account(self, account_id: int) -> Account:
         account = self.db.query(Account).filter(Account.id == account_id).first()
         if not account:
-            raise HTTPException(404, "Account not found")
+            raise AccountNotFound()
         return account
 
     def get_account_by_user_id(self, user_id: int) -> Account:
         account = self.db.query(Account).filter(Account.user_id == user_id).first()
         if not account:
-            raise HTTPException(404, "Account not found for this user")
+            raise AccountNotFound()
         return account
 
     def deposit(self, account_id: int, amount: float):
         if amount <= 0:
-            raise HTTPException(400, "Amount must be positive")
+            raise InvalidAmount()
 
         account = self.get_account(account_id)
         account.balance += amount
 
-        transaction = Transaction(
-            account_id=account.id,
-            amount=amount,
-            transaction_type=TransactionType.DEPOSIT,
-            transaction_date=datetime.utcnow()
-        )
+        transaction_service = TransactionService(self.db)
+        transaction = transaction_service.save_transaction(account_id, amount, TransactionType.DEPOSIT)
 
-        self.db.add(transaction)
         self.db.commit()
         self.db.refresh(account)
 
@@ -44,42 +40,43 @@ class AccountService:
 
     def withdraw(self, account_id: int, amount: float):
         if amount <= 0:
-            raise HTTPException(400, "Amount must be positive")
+            raise InvalidAmount
 
         account = self.get_account(account_id)
+        transaction_service = TransactionService(self.db)
 
         if account.balance < amount:
-            raise HTTPException(status_code=400, detail="Insufficient funds")
+            raise InsufficientFunds()
 
         account.balance -= amount
 
-        transaction = Transaction(
-            account_id=account.id,
-            amount=amount,
-            transaction_type=TransactionType.WITHDRAWAL,
-            transaction_date=datetime.utcnow()
-        )
+        transaction = transaction_service.save_transaction(account_id, amount, TransactionType.WITHDRAWAL)
 
-        self.db.add(transaction)
         self.db.commit()
         self.db.refresh(account)
 
         return account
 
     def get_transactions(self, account_id: int):
-        account = self.get_account(account_id)
+        # account = self.get_account(account_id)
         return self.db.query(Transaction).filter(Transaction.account_id == account_id).order_by(
             Transaction.transaction_date).all()
-    
-    def all_transaction(self):
+
+    def all_transactions(self):
         return self.db.query(Transaction).order_by(
             Transaction.transaction_date).all()
-    
 
+    def create_account(self, user_id: int, initial_balance: float = 0.0) -> Account:
 
-    def create_account(self, user_id: int) -> Account:
-        account = Account(user_id=user_id, _balance=0.0)
+        if initial_balance < 0:
+            raise InvalidAmount(initial_balance, "Initial balance cannot be negative")
+
+        account = Account(user_id=user_id, _balance=initial_balance)
         self.db.add(account)
-        self.db.commit()
-        self.db.refresh(account)
+        self.db.flush()
+
+        if initial_balance > 0:
+            transaction_service = TransactionService(self.db)
+            transaction_service.save_transaction(account.id, initial_balance, TransactionType.DEPOSIT)
+
         return account
