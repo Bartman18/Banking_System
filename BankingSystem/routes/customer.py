096fd4372da5251837552e9505162e672c832f21
Bartosz Iwanicki <bartosz.iwanickii@gmail.com>
2026-02-01 22:54:00 +0100

    fix: routes method

--- BankingSystem/routes/customer.py
+++ BankingSystem/routes/customer.py
@@ -1,60 +1,65 @@
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, HTTPException
+from security.exception import AccountNotFound, InvalidAmount, InsufficientFunds
 from service.user_service import UserService
 from security.deps import require_client, get_current_user
 from db.database_dependencies import get_db
 from sqlalchemy.orm import Session
-from schemas.transaction import TransactionType
+from schemas.transaction import TransactionType, TransactionOut
 from schemas.user import UserResponse
 from service.account_service import AccountService
 from models.user import User
-
+from typing import List
 
 router = APIRouter(prefix="/customer", tags=["Customer"])
 
 
-
-
-
-
 @router.post('/transaction/')
-def transaction(data:TransactionType ,amount:float, db:Session = Depends(get_db), user: User = Depends(require_client)):
-    
+def transaction(data: TransactionType, amount: float, db: Session = Depends(get_db),
+                user: User = Depends(require_client)):
     service = AccountService(db)
     account = service.get_account_by_user_id(user.id)
+    try:
+        if data == TransactionType.WITHDRAWAL:
+            return service.withdraw(account.id, amount)
+        elif data == TransactionType.DEPOSIT:
+            return service.deposit(account.id, amount)
+        else:
+            raise HTTPException(400, "Invalid transaction type")
+    except AccountNotFound:
+        raise HTTPException(400, "Account not found")
+    except InvalidAmount:
+        raise HTTPException(400, "Invalid amount")
+    except InsufficientFunds:
+        raise HTTPException(400, "Insufficient funds")
 
-    if data == TransactionType.WITHDRAWAL:
-        return service.withdraw(account.id, amount)
-    elif data == TransactionType.DEPOSIT:
-        return service.deposit(account.id, amount)
 
-    else:
-        raise HTTP_401_UNAUTHORIZED
+@router.get('/transaction_history/', response_model=List[TransactionOut])
+def transaction_history(db: Session = Depends(get_db), user: User = Depends(require_client)):
+    try:
+        service = AccountService(db)
+        account = service.get_account_by_user_id(user.id)
+        transactions = service.get_transactions(account.id)
 
-
-@router.get('/transaction_history/')
-def transaction_history(db:Session = Depends(get_db),user: User = Depends(require_client)):
-    service = AccountService(db)
-    account = service.get_account(user.id)
-    transaction = service.get_transactions(account.id)
-    return [
-        {
-            "id":t.id,
-            "amount":t.amount,
-            "type":t.transaction_type.value,
-            "date":t.transaction_date
-        }
-        for t in transaction
-    ]
+        transactions_out = [
+            TransactionOut(
+                id=t.id,
+                # account_id=t.account_id,
+                amount=t.amount,
+                transaction_type=t.transaction_type,
+                transaction_date=t.transaction_date,
+                user_name=t.account.user.full_name()
+            )
+            for t in transactions
+        ]
+        return transactions_out
+    except AccountNotFound:
+        raise HTTPException(404, "Account not found")
 
 
 @router.get('/current_balance/')
-def get_balance(db:Session = Depends(get_db), user:User = Depends(require_client)):
-    account = AccountService(db).get_account_by_user_id(user.id)
-    return {"balance":account.balance}
-    
-    
-
-
-@router.get('/statment/')
-def statment():
-    pass
+def get_balance(db: Session = Depends(get_db), user: User = Depends(require_client)):
+    try:
+        account = AccountService(db).get_account_by_user_id(user.id)
+        return {"balance": account.balance}
+    except AccountNotFound:
+        raise HTTPException(404, "Account not found")
--- BankingSystem/routes/manager.py
+++ BankingSystem/routes/manager.py
@@ -1,81 +1,103 @@
 from fastapi import Depends, APIRouter, HTTPException
+
+from schemas.transaction import TransactionOut
+from security.exception import UserAlreadyExists, InvalidAmount, UserNotFound, AccountNotFound
 from service.user_service import UserService
 from service.account_service import AccountService
 from db.database_dependencies import get_db
 from sqlalchemy.orm import Session
-from schemas.user import UserCreate,UserResponse
+from schemas.user import UserCreate, UserResponse
 from security.deps import require_manager
-router = APIRouter(prefix="/manager", tags=["Manager"]), dependencies=[Depends(require_manager)]
+from typing import List
+
+router = APIRouter(prefix="/manager", tags=["Manager"], dependencies=[Depends(require_manager)])
 
 
 @router.delete("/delete/{customer_id}")
-def delete_customer(user_id:int, db:Session = Depends(get_db)):
-    
-    UserService(db).delete_user(user_id)
-    
+def delete_customer(customer_id: int, db: Session = Depends(get_db)):
+    try:
+        UserService(db).delete_user(customer_id)
+        return {"message": "User deleted"}
+    except UserNotFound:
+        raise HTTPException(404, "User not found")
 
 
-@router.post('create/user/')
-def add_client(data:UserCreate, db:Session = Depends(get_db)):
-    service = UserService(db)
+@router.post('/create/user/')
+def add_client(data: UserCreate, db: Session = Depends(get_db)):
+    try:
+        service = UserService(db)
+        user = service.create_user(data)
 
-    if service.get_by_email(data.email):
-        raise HTTPException(400, "Email is already registered")
+        return UserResponse(
+            id=user.id,
 
-    user = service.create_user(data)
-
-    return UserResponse(
-        id=user.id,
-        
-        email=user.email,
-        role=user.role,
-        full_name = user.full_name()
-
-    )
-
-@router.get("/{customer_id}")
-def get_user(user_id:int ,db:Session = Depends(get_db)):
-    user = UserService(db).get_by_id(user_id)
-    if not user:
-        raise HTTPException(400, "User not exist")
-    return UserResponse(
-        id=user.id,
-        email=user.email,
-        role=user.role,
-        full_name = user.full_name()
-
-    )
+            email=user.email,
+            role=user.role,
+            full_name=user.full_name()
+        )
+    except UserAlreadyExists:
+        raise HTTPException(400, detail="User already exists")
+    except Exception as e:
+        raise HTTPException(500)
 
 
-@router.get('/all_transaction/')
-def all_transaction(db:Session = Depends(get_db)):
-    service = AccountService(db)
-    transaction = service.all_transaction()
-    return [
-        {
-            "id":t.id,
-            "account_id":t.account_id,
-            "amount":t.amount,
-            "type":t.transaction_type.value,
-            "date":t.transaction_date
-        }
-        for t in transaction
-    ]
-    
+@router.get('/all_transactions_list/', response_model=List[TransactionOut])
+def all_transaction(db: Session = Depends(get_db)):
+    try:
+        service = AccountService(db)
+        transactions = service.all_transactions()
+
+        transactions_out = [
+            TransactionOut(
+                id=t.id,
+                # account_id=t.account_id,
+                amount=t.amount,
+                transaction_type=t.transaction_type,
+                transaction_date=t.transaction_date,
+                user_name=t.account.user.full_name()
+            )
+            for t in transactions
+        ]
+        return transactions_out
+    except AccountNotFound:
+        raise HTTPException(404, "Account not found")
+
+
+
+
+@router.get("/{customer_id}/")
+def get_user(customer_id: int, db: Session = Depends(get_db)):
+    try:
+
+        user = UserService(db).get_by_id(customer_id)
+        return UserResponse(
+            id=user.id,
+            email=user.email,
+            role=user.role,
+            full_name=user.full_name()
+
+        )
+    except UserNotFound:
+        raise HTTPException(404, "User not found")
+
+
 @router.post("/all_customers/")
-def get_all_client(db:Session = Depends(get_db)):
-    user = UserService(db).get_all_users()
-    return [
-        UserResponse(
-            id=x.id,
-            email=x.email,
-            role=x.role,
-            full_name = x.full_name()
+def get_all_client(db: Session = Depends(get_db)):
+    service = UserService(db)
+    users = service.get_all_users()
 
-        ) for x in user
-    ]
-@router.post("/")
-def balanace():
-    pass
+    result = []
+    for u in users:
+        account = service.get_user_account(u.id)
+        result.append({
+            "id": u.id,
+            "name": u.name,
+            "surname": u.surname,
+            "email": u.email,
+            "role": u.role.value,
+            "full_name": u.full_name(),
+            "account_id": account.id if account else None,
+            "balance": account.balance if account else None
+        })
 
-
+    return result
