b8817bc8e6064c04426f5c8cca623b417c96fc0c
Bartosz Iwanicki <bartosz.iwanickii@gmail.com>
2026-02-01 22:57:52 +0100

    fix: user_service fixed

--- BankingSystem/service/user_service.py
+++ BankingSystem/service/user_service.py
@@ -3,6 +3,8 @@ from models.user import User
 from models.account import Account
 from security.security import get_password_hash, verify_password
 from schemas.user import UserResponse
+from security.exception import UserAlreadyExists, InvalidAmount, UserNotFound
+from service.account_service import AccountService
 
 
 class UserService:
@@ -16,25 +18,38 @@ class UserService:
         return self.db.query(User).filter(User.email == email).first()
 
     def delete_user(self, user_id: int):
-        
-        self.db.query(User).filter(User.id == user_id).delete()
+        user = self.db.query(User).filter(User.id == user_id).first()
+
+        if not user:
+            raise UserNotFound()
+
+        self.db.delete(user)
         self.db.commit()
 
-
     def create_user(self, data):
+        existing_user = self.get_by_email(data.email)
+        if existing_user:
+            raise UserAlreadyExists()
+
+        initial_balance = data.initial_balance
+        if initial_balance < 0:
+            raise InvalidAmount(initial_balance, "Initial balance cannot be negative")
+
         user = User(
             name=data.name,
             surname=data.surname,
             email=data.email,
             hashed_password=get_password_hash(data.password),
             role=data.role,
-            initial_balance = data.initial_balance
+            initial_balance=data.initial_balance
         )
+
         self.db.add(user)
         self.db.flush()
 
-        account = Account(user_id=user.id, _balance=user.initial_balance)
-        self.db.add(account)
+        account_service = AccountService(self.db)
+        account = account_service.create_account(user.id, data.initial_balance)
+
 
         self.db.commit()
         self.db.refresh(user)
@@ -51,3 +66,6 @@ class UserService:
     def get_all_users(self):
         user = self.db.query(User).all()
         return user
+
+    def get_user_account(self, user_id: int) -> Account | None:
+        return self.db.query(Account).filter(Account.user_id == user_id).first()
